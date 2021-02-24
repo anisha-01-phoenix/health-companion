a4417bec17ca52962b320a19a593663a1101ad1f
anisha-01-phoenix <79036525+anisha-01-phoenix@users.noreply.github.com>
2021-02-24 20:34:42 +0530

    health-companion

--- app/src/main/java/com/example/healthpal/Activity1.java
+++ app/src/main/java/com/example/healthpal/Activity1.java
@@ -1,30 +1,90 @@
 package com.example.healthpal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity1 extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
 
+        EditText txtName= findViewById(R.id.txt_name);
+
+        EditText txtEmail= findViewById(R.id.txt_email);
+        EditText txtPassword= findViewById(R.id.txt_password);
+        EditText txtConfirmPassword= findViewById(R.id.txt_confirmpassword);
         Button button3=findViewById(R.id.button3);
+        firebaseAuth=FirebaseAuth.getInstance();
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDashboard();
+
+                String email= txtEmail.getText().toString().trim();
+                String password=txtPassword.getText().toString().trim();
+                String name=txtName.getText().toString().trim();
+                String ConfirmPassword=txtConfirmPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(name))
+                {
+                    Toast.makeText(Activity1.this,"Enter Name",Toast.LENGTH_SHORT) .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(Activity1.this,"Enter Email id",Toast.LENGTH_SHORT) .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(Activity1.this,"Enter Password",Toast.LENGTH_SHORT) .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(ConfirmPassword))
+                {
+                    Toast.makeText(Activity1.this,"Confirm Password",Toast.LENGTH_SHORT) .show();
+                    return;
+                }
+                if(password.length()<8)
+                {
+                    Toast.makeText(Activity1.this,"Password must contain atleast 8 characters",Toast.LENGTH_SHORT) .show();
+                    return;
+                }
+                if(password.equals(ConfirmPassword))
+                {
+                    firebaseAuth.createUserWithEmailAndPassword(email,password)
+                            .addOnCompleteListener(Activity1.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful())
+                                    {
+                                        startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                                        Toast.makeText(Activity1.this,"Welcome to Healthpal!",Toast.LENGTH_SHORT) .show();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(Activity1.this, "Authentication Failed!", Toast.LENGTH_SHORT) .show();
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
-    public void openDashboard()
-    {
-        Intent intent=new Intent(this, Dashboard.class);
-        startActivity(intent);
-    }
+
 }
--- app/src/main/java/com/example/healthpal/Activity2.java
+++ app/src/main/java/com/example/healthpal/Activity2.java
@@ -1,20 +1,73 @@
 package com.example.healthpal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity2 extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+
         Button button= findViewById(R.id.button);
+        EditText txtEmail=findViewById(R.id.txt_Email);
+        EditText txtPassword=findViewById(R.id.txt_Password);
+        Button btn_login=findViewById(R.id.btn_login);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=txtEmail.getText().toString().trim();
+                String password=txtPassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(Activity2.this,"Enter Email id",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(Activity2.this,"Enter Password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.length()<8)
+                {
+                    Toast.makeText(Activity2.this,"Password must contain at least 8 characters",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                firebaseAuth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(Activity2.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    startActivity(new Intent(getApplicationContext(),Dashboard.class));
+                                    Toast.makeText(Activity2.this,"Successfully Logged In!",Toast.LENGTH_SHORT) .show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(Activity2.this, "Login Failed!", Toast.LENGTH_SHORT) .show();
+                                }
+                            }
+                        });
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
